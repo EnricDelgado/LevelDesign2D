aaeee27b08982f9ceb0058b599f71efe02b3fab9
RunningLate <enricdelgadoclaramunt@gmail.com>
2020-05-17 01:22:20 +0200

    Deadzone controller

---
+++ Assets/Scripts/DeadZoneController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeadZoneController : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other) {
+        if(other.tag == "Player")
+            GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerStats>().HP = 0;
+    }
+}
---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    public float HP = 100;
+
+    void Update()
+    {
+        if(HP <= 0)
+            Destroy(gameObject);
+    }
+}
