1986f11f6101ddfb70a9cffa621aafd2107b8880
RunningLate <enricdelgadoclaramunt@gmail.com>
2020-05-17 01:54:55 +0200

    checkpoint implementation

---
+++ Assets/Scripts/CheckPointController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPointController : MonoBehaviour
+{
+    Vector3 checkPointPos = new Vector3();
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if(other.tag == "Player")
+            GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerStats>().CheckPoint = this.transform.position;
+    }
+}
--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -5,10 +5,40 @@ using UnityEngine;
 public class PlayerStats : MonoBehaviour
 {
     public float HP = 100;
+    public Vector3 CheckPoint = new Vector3();
+
+    void Start() {
+        CheckPoint = transform.position;
+    }
 
     void Update()
     {
+        CheckPlayerDeath();
+    }
+
+    void CheckPlayerDeath()
+    {
         if(HP <= 0)
-            Destroy(gameObject);
+        {
+            transform.position = CheckPoint;
+            HP = 100;
+            StartCoroutine(PlayerCooldown());
+        }
+    }
+
+    IEnumerator PlayerCooldown()
+    {
+        GameObject Player = GameObject.FindGameObjectWithTag("Player");
+
+        float initialSpeed = Player.GetComponent<PlayerMovement>().runSpeed;
+        float initialJumpF = Player.GetComponent<CharacterController2D>().m_JumpForce;
+
+        Player.GetComponent<PlayerMovement>().runSpeed = 0;
+        Player.GetComponent<CharacterController2D>().m_JumpForce = 0;
+
+        yield return new WaitForSeconds(.5f);
+
+        Player.GetComponent<PlayerMovement>().runSpeed = initialSpeed;
+        Player.GetComponent<CharacterController2D>().m_JumpForce = initialJumpF;
     }
 }
