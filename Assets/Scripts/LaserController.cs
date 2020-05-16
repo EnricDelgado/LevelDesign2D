cf37e456299b68a28e770b0cf9e412cd3eb2f77e
RunningLate <enricdelgadoclaramunt@gmail.com>
2020-05-16 21:16:04 +0200

    Laser implementation

---
+++ Assets/Scripts/LaserController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserController : MonoBehaviour
+{
+    public GameObject Laser;
+    public bool isRunning;
+
+    void Start() 
+    {
+        isRunning = false;    
+    }
+
+    void Update()
+    {
+        if(!isRunning)
+        {
+            Debug.Log("IF entered");
+            isRunning = true;
+            Debug.Log("Entering corroutine");
+            StartCoroutine(DisplayLaser(Laser));
+        }
+    }
+
+    IEnumerator DisplayLaser(GameObject laser)
+    {
+        Debug.Log("Running Corroutine");
+        laser.gameObject.SetActive(true);
+        yield return new WaitForSeconds(2.5f);
+        laser.gameObject.SetActive(false);
+        yield return new WaitForSeconds(2.5f);
+        laser.gameObject.SetActive(true);
+        isRunning = false;
+        Debug.Log("Ending corroutine");
+    }
+}
