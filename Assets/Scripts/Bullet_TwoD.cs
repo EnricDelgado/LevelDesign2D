b94890d6c874eca1638eec7e6b72c278db2124d8
RubensPD <50207045+RubensPD@users.noreply.github.com>
2020-05-21 16:14:10 +0200

    turret script

---
+++ Assets/Scripts/Bullet_TwoD.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet_TwoD : MonoBehaviour
+
+{
+    public float bulletSpeed;
+
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = transform.up * bulletSpeed * 10 * Time.deltaTime;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag =="Player" || collision.gameObject.tag =="Wall" || collision.gameObject.tag =="Ground" || collision.gameObject.tag =="BreakableDoor" || collision.gameObject.tag == "EnemyBomb")
+        Destroy(this.gameObject);
+        
+    }
+}
---
+++ Assets/Scripts/Turret_TwoD.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret_TwoD : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject bullet;
+    public GameObject player;
+    public GameObject canon;
+
+
+    public bool isDestroyable;
+
+    public float distanciaDisparo = 10;
+    public float frequency = 2;
+    public int health = 2;
+    private float timer;
+    public Transform disparoPoint;
+    private float angulo;
+    /*float maxAngleLeft;
+    float maxAngleRight;
+    float maxAngleUp;
+    float maxAngleDown;
+    float minxAngleLeft;
+    float minAngleRight;
+    float minAngleUp;
+    float minAngleDown;*/
+
+    void Start()
+    {
+      /*  maxAngleLeft = 90f;
+        maxAngleRight = -90f;
+        maxAngleUp = -180f;
+        maxAngleDown = 180f;*/
+        timer = frequency;
+        //player = GameObject.FindGameObjectWithTag("Player");
+        
+    }
+
+    void Update()
+    {
+
+        //PILLAMOS ANGULO HACIA PLAYER
+        
+        angulo = Mathf.Atan2(player.transform.position.y - canon.transform.position.y, player.transform.position.x - canon.transform.position.x) * Mathf.Rad2Deg - 90;
+
+        //ROTAMOS CAÑON      
+
+        Debug.Log(angulo);
+       
+       
+        
+        
+        
+        
+
+       
+        
+            canon.transform.rotation = Quaternion.Euler(new Vector3(0, 0, angulo));
+        
+        //DISPARAMOS
+        timer -= Time.deltaTime;
+        if (timer < 0)
+        {
+            if (Vector2.Distance(player.transform.position, transform.position) < distanciaDisparo)
+            {
+             
+                Instantiate(bullet, disparoPoint.position, canon.transform.rotation);
+                
+
+            }
+
+            timer = frequency;
+        }
+    
+        if (health <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Attack") && isDestroyable == true)
+        {
+            health--;
+        }
+    }
+}
